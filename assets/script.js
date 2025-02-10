f16b309b712e79c18b3d2584b4ba71013949e2ea
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-02-10 17:28:07 +0100

    Wrapped the DOM elements in a cacheDom object

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,19 @@
 const Players = (function() {
-    const playerSelector = document.querySelector(".player-selector");
-    const playBtn = document.querySelector("#play-btn");
-    const inputPlayer1 = document.querySelector("#player1");
-    const inputPlayer2 = document.querySelector("#player2");
+    const cacheDom = {
+        playerSelector: document.querySelector(".player-selector"),
+        playBtn: document.querySelector("#play-btn"),
+        inputPlayer1: document.querySelector("#player1"),
+        inputPlayer2: document.querySelector("#player2"),
+    };
 
     let player1, player2, currentPlayer;
 
     function showSelector() {
-        playerSelector.classList.remove("hidden");
+        cacheDom.playerSelector.classList.remove("hidden");
     }
 
     function hideSelector() {
-        playerSelector.classList.add("hidden");
+        cacheDom.playerSelector.classList.add("hidden");
     }
 
     function namePlayer(name, marker) {
@@ -22,13 +24,13 @@ const Players = (function() {
     }
 
     function resetInputs() {
-        inputPlayer1.value = "";
-        inputPlayer2.value = "";
+        cacheDom.inputPlayer1.value = "";
+        cacheDom.inputPlayer2.value = "";
     }
 
     function setPlayers() {
-        player1 = namePlayer(inputPlayer1.value || "Player 1", "cross");
-        player2 = namePlayer(inputPlayer2.value || "Player 2", "circle");
+        player1 = namePlayer(cacheDom.inputPlayer1.value || "Player 1", "cross");
+        player2 = namePlayer(cacheDom.inputPlayer2.value || "Player 2", "circle");
         currentPlayer = player1;
     }
 
@@ -41,7 +43,7 @@ const Players = (function() {
         return currentPlayer;
     }
 
-    playBtn.addEventListener("click", () => {
+    cacheDom.playBtn.addEventListener("click", () => {
         hideSelector();
         setPlayers();
         Board.showBoard();
